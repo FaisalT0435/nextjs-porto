daab6b270f156c75470e89f9225f240fd715abfa
Faisal <faisaltsurya@gmail.com>
2025-03-13 12:34:04 +0700

    chatbot use openAI

--- src/components/api/openai.ts
+++ src/components/api/openai.ts
@@ -1,7 +1,7 @@
 import OpenAI from 'openai';
 
 const openai = new OpenAI({
-  apiKey: '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', // defaults to process.env["OPENAI_API_KEY"]
+  apiKey: 'xxx', // defaults to process.env["OPENAI_API_KEY"]
 });
 
 
