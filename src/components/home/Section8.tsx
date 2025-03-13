4b8fdb54a6a6b5f774e0bfdd896e44f1918f995d
Faisal <faisaltsurya@gmail.com>
2025-03-13 10:38:41 +0700

    test chatbot v-2

--- src/components/home/Section8.tsx
+++ src/components/home/Section8.tsx
@@ -1,6 +1,6 @@
 import Chatbot from '@/components/chatbot/chatbot';
 
-const HomeSection8 = () => {
+const HomeSection8 = ({ id }: { id: string }) => {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
       <Chatbot/>
